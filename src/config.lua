9cae34e52cbb85d92b3c4671dda881232d4445a9
FV214Hardiel <92205042+FV214Hardiel@users.noreply.github.com>
2024-06-13 16:56:03 +0300

    Config fix

--- src/config.lua
+++ src/config.lua
@@ -2,5 +2,14 @@ local config = {
   Enabled = true,
   FreeArcanas = true,
   MoreRerolls = true,
-  RerollCount = 12,
+  RerollCount = 9,
 }
+
+local desc = {
+  Enabled = "True for enable mod, false for disable",
+  FreeArcanas = "True for 0 grasp cost for every reroll card, false for default values",
+  MoreRerolls = "True for custom base amount of rerolls for cards, false for default values",
+  RerollCount = "Sets base value if MoreRerolls is enabled"
+}
+
+return config, desc
